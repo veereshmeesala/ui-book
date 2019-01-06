7f05148c1e8e2bd65abcf1171bcf413926bfe086
veereshmeesala <veereshmeesala@gmail.com>
2019-01-05 19:45:04 -0500

    tooling setup files

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var chalk = require('chalk');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 
-app.get('/', function(req, res) {
-    // res.send('hello from my library app');
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', (req, res) => {
+  // res.send('hello from my library app');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function() {
-    console.log(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  console.log(`listening on port ${chalk.green(port)}`);
 });
