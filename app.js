ed6ad5fab169fb6a77b3854285e983ed03284820
veereshmeesala <veereshmeesala@gmail.com>
2019-01-09 20:11:39 -0500

    ejs templating

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   // res.send('hello from my library app');
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
